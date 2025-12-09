5afdc8dd7dd37d98c5658bb7d9597b9c3eb3f32d
Anupama Kittur <anupama.kittur@gmail.com>
2025-12-09 19:23:09 +1100

    added code for generate_summary function

--- example.py
+++ example.py
@@ -1,4 +1,4 @@
-from weather import format_temperature , convert_date, convert_f_to_c, calculate_mean, load_data_from_csv, find_min, find_max
+from weather import format_temperature , convert_date, convert_f_to_c, calculate_mean, load_data_from_csv, find_min, find_max, generate_summary
 
 # print(format_temperature(30)) # call function which is present in weather.py
 
@@ -12,8 +12,9 @@ from weather import format_temperature , convert_date, convert_f_to_c, calculate
 # csv_file = "tests\data\example_two.csv"
 # print(load_data_from_csv(csv_file))
 
-# print(find_min(weather_data=[]))
+# print(find_min(weather_data=[10,20,30,10]))
 
 # print(find_max(weather_data=[49, 57, 56, 55, 53]))
 
-
+# weather_data = [['2020-06-19T07:00:00+08:00', 47, 46], ['2020-06-20T07:00:00+08:00', 51, 67] , ['2020-06-21T07:00:00+08:00', 58, 72], ['2020-06-22T07:00:00+08:00', 59, 71], ['2020-06-23T07:00:00+08:00', 52, 71], ['2020-06-24T07:00:00+08:00', 52, 67], ['2020-06-25T07:00:00+08:00', 48, 66], ['2020-06-26T07:00:00+08:00', 53, 66]]
+# print(generate_summary(weather_data))
